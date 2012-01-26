52bd12eaed5fb21dc0a7003fdef7db7acd35568f
AD7six <andydawson76@gmail.com>
2012-01-26 12:37:33 +0100

    add option autocomplete
    restructure to make it easier to be expicit. add fuzzy stub

--- Console/Command/BashCompletionShell.php
+++ Console/Command/BashCompletionShell.php
@@ -12,22 +12,110 @@ class BashCompletionShell extends CommandListShell {
 	}
 
 	/**
-	 * If there are no args - list all commands
-	 * If there is one arg - it's the name of the command to run. list subcommands
-	 *
+	 * Not called by the autocomplet shell - this is for curious users
 	 */
 	public function main() {
-		$options = array();
+		$this->out($this->OptionParser->help());
+	}
 
+	/**
+	 * list commands
+	 */
+	public function commands() {
+		$options = $this->_commands();
+		return $this->output($options);
+	}
+
+	/**
+	 * list options for the named command
+	 */
+	public function options() {
 		if (!$this->args) {
-			$options = $this->commands();
+			$parser = new ConsoleOptionParser();
+		} else {
+			$Shell = $this->_getShell($this->args[0]);
+			if (!$Shell) {
+				$parser = new ConsoleOptionParser();
+			} else {
+				$parser = $Shell->getOptionParser();
+			}
 		}
 
-		if (count($this->args) === 1) {
-			$options = $this->subCommands($this->args[0]);
+		$options = array();
+		$array = $parser->options();
+		foreach($array as $name => $obj) {
+			$options[] = "--$name";
+			$short = $obj->short();
+			if ($short) {
+				$options[] = "-$short";
+			}
+		}
+		return $this->output($options);
+	}
+
+	/**
+	 * list subcommands for the named command
+	 */
+	public function subCommands() {
+		if (!$this->args) {
+			return $this->output();
 		}
 
-		$this->output($options);
+		$options = $this->_subCommands($this->args[0]);
+		return $this->output($options);
+	}
+
+	/**
+	 * Guess autocomplete from the whole argument string
+	 */
+	public function fuzzy() {
+		return $this->output();
+	}
+
+	/**
+	 * getOptionParser for _this_ shell
+	 */
+	public function getOptionParser() {
+		$translationDomain = 'bash_completion';
+
+		$parser = AppShell::getOptionParser();
+
+		$parser->description(__d($translationDomain, 'Used by bash to autocomplete command name, options and arguments'))
+			->addSubcommand('commands', array(
+				'help' => __d($translationDomain, 'Output a list of available commands'),
+				'parser' => array(
+					'description' => __d($translationDomain, 'List all availables'),
+					'arguments' => array(
+					)
+				)
+			))->addSubcommand('subcommands', array(
+				'help' => __d($translationDomain, 'Output a list of available subcommands'),
+				'parser' => array(
+					'description' => __d($translationDomain, 'List subcommands for a command'),
+					'arguments' => array(
+						'command' => array(
+							'help' => __d($translationDomain, 'The command name'),
+							'required' => true,
+						)
+					)
+				)
+			))->addSubcommand('options', array(
+				'help' => __d($translationDomain, 'Output a list of available options'),
+				'parser' => array(
+					'description' => __d($translationDomain, 'List options'),
+					'arguments' => array(
+						'command' => array(
+							'help' => __d($translationDomain, 'The command name'),
+							'required' => false,
+						)
+					)
+				)
+			))->epilog(
+				array(
+					'This command is not intended to be called manually',
+				)
+			);
+		return $parser;
 	}
 
 	/**
@@ -35,7 +123,7 @@ class BashCompletionShell extends CommandListShell {
 	 *
 	 * @return array
 	 */
-	public function commands() {
+	protected function _commands() {
 		$shellList = $this->_getShellList();
 
 		$options = array();
@@ -48,7 +136,7 @@ class BashCompletionShell extends CommandListShell {
 					$prefix = $type . '.';
 				}
 			}
-			$options[] = $prefix . $shell;
+			$options[] = Inflector::variable($prefix . $shell);
 		}
 
 		return $options;
@@ -60,7 +148,37 @@ class BashCompletionShell extends CommandListShell {
 	 * @param string $commandName
 	 * @return array
 	 */
-	public function subcommands($commandName) {
+	protected function _subCommands($commandName) {
+		$Shell = $this->_getShell($commandName);
+
+		if (!$Shell) {
+			return array();
+		}
+
+		$return = array_map('Inflector::variable', $Shell->tasks);
+
+		$ShellReflection = new ReflectionClass('AppShell');
+		$shellMethods = $ShellReflection->getMethods(ReflectionMethod::IS_PUBLIC);
+		$shellMethodNames = array('main', 'help');
+		foreach($shellMethods as $method) {
+			$shellMethodNames[] = Inflector::variable($method->getName());
+		}
+
+		$Reflection = new ReflectionClass($Shell);
+		$methods = $Reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+		$methodNames = array();
+		foreach($methods as $method) {
+			$methodNames[] = Inflector::variable($method->getName());
+		}
+
+		$return += array_diff($methodNames, $shellMethodNames);
+
+		sort($return);
+
+		return $return;
+	}
+
+	protected function _getShell($commandName) {
 		list($plugin, $name) = pluginSplit($commandName, true);
 
 		$underscored = Inflector::underscore($name);
@@ -79,28 +197,10 @@ class BashCompletionShell extends CommandListShell {
 
 		$Shell = new $class();
 		$Shell->plugin = trim($plugin, '.');
+		$Shell->initialize();
+		$Shell->loadTasks();
 
-		$return = $Shell->tasks;
-
-		$ShellReflection = new ReflectionClass('AppShell');
-		$shellMethods = $ShellReflection->getMethods(ReflectionMethod::IS_PUBLIC);
-		$shellMethodNames = array('main', 'help');
-		foreach($shellMethods as $method) {
-			$shellMethodNames[] = $method->getName();
-		}
-
-		$Reflection = new ReflectionClass($Shell);
-		$methods = $Reflection->getMethods(ReflectionMethod::IS_PUBLIC);
-		$methodNames = array();
-		foreach($methods as $method) {
-			$methodNames[] = $method->getName();
-		}
-
-		$return += array_diff($methodNames, $shellMethodNames);
-
-		sort($return);
-
-		return $return;
+		return $Shell;
 	}
 
 	/**
@@ -108,7 +208,7 @@ class BashCompletionShell extends CommandListShell {
 	 *
 	 * @param array $options
 	 */
-	public function output($options) {
+	protected function output($options = array()) {
 		if ($options) {
 			$this->out(implode($options, ' '));
 		}
--- Vendor/cake.bash
+++ Vendor/cake.bash
@@ -4,20 +4,39 @@ _cake()
 {
     local cur prev opts cake
     COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
     cake="${COMP_WORDS[0]}"
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    if [[ ${COMP_CWORD} = 1 ]] ; then
-    	opts=$(${cake} BashCompletion.BashCompletion)
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-        return 0
-    fi
-    if [[ ${COMP_CWORD} = 2 ]] ; then
-    	opts=$(${cake} BashCompletion.BashCompletion $prev)
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-        return 0
-    fi
+	if [[ "$cur" == -* ]] ; then
+		if [[ ${COMP_CWORD} = 1 ]] ; then
+			opts=$(${cake} BashCompletion.BashCompletion options)
+		elif [[ ${COMP_CWORD} = 2 ]] ; then
+			opts=$(${cake} BashCompletion.BashCompletion options "${COMP_WORDS[1]}")
+		else
+			opts=$(${cake} BashCompletion.BashCompletion options "${COMP_WORDS[1]}" "${COMP_WORDS[2]}")
+		fi
+
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		return 0
+	fi
+
+	if [[ ${COMP_CWORD} = 1 ]] ; then
+		opts=$(${cake} BashCompletion.BashCompletion commands)
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		return 0
+	fi
+
+	if [[ ${COMP_CWORD} = 2 ]] ; then
+		opts=$(${cake} BashCompletion.BashCompletion subcommands $prev)
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		return 0
+	fi
+
+
+    opts=$(${cake} BashCompletion.BashCompletion fuzzy "${COMP_WORDS[@]:1}")
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	return 0;
 
 }
 complete -F _cake cake cakephp
