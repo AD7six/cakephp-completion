7c35810c936116bfa1fb932d86ece85b7b77b660
AD7six <andydawson76@gmail.com>
2012-10-08 08:55:18 +0200

    complete file/dir names
    If the complete list is empty - treat the input argument as a
    folder/filename

--- Vendor/cake.bash
+++ Vendor/cake.bash
@@ -30,13 +30,21 @@ _cake()
 	if [[ ${COMP_CWORD} = 2 ]] ; then
 		opts=$(${cake} Completion.Completion subcommands $prev)
 		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		if [[ $COMPREPLY = "" ]] ; then
+			COMPREPLY=( $(compgen -df -- ${cur}) )
+			return 0
+		fi
 		return 0
 	fi
 
 
     opts=$(${cake} Completion.Completion fuzzy "${COMP_WORDS[@]:1}")
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	if [[ $COMPREPLY = "" ]] ; then
+		COMPREPLY=( $(compgen -df -- ${cur}) )
+		return 0
+	fi
 	return 0;
-
 }
-complete -F _cake cake cakephp
+
+complete -F _cake cake Console/cake
